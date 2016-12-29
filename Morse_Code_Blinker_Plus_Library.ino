925d176750b90ad478a7c9fd0ee54511b3bc6ef0
Brave Tea <Alexvthooft@gmail.com>
2016-12-29 21:18:46 +0100

    Add files via upload

---
+++ Morse_Code_Blinker_Plus_Library.ino
@@ -0,0 +1,22 @@
+#include "Morse.h"
+
+void setup()
+{
+  pinMode(obLed, OUTPUT);
+}
+
+void loop()
+{
+  b();
+  a();
+  c();
+  h();
+  space();
+  r();
+  u();
+  l();
+  e();
+  s();
+  space();
+}
+
